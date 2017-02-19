b557bc993b267ceab9869a4458d14d48360ef5bc
Alfred Foltin <afoltin@mobiquityinc.com>
2017-02-19 19:24:22 +0100

    headers in mock response being de-serialised correctly now

--- garnish-steps/garnish-steps-mockhttpserver/src/main/java/com/mobiquityinc/mobit/steps/mockhttpserver/HttpMocksSteps.java
+++ garnish-steps/garnish-steps-mockhttpserver/src/main/java/com/mobiquityinc/mobit/steps/mockhttpserver/HttpMocksSteps.java
@@ -2,13 +2,15 @@ package com.mobiquityinc.mobit.steps.mockhttpserver;
 
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.mobiquityinc.mobit.modules.generic.springutils.ClasspathUtils;
-import com.mobiquityinc.mobit.modules.it.test_utils_json.JsonUtils;
 import com.mobiquityinc.mobit.steps.mockhttpserver.server.HttpMockServerManager;
 import com.mobiquityinc.mobit.steps.vars.resource_files_vars.ResourceFilesVariables;
 import cucumber.api.java.en.Given;
 import lombok.NonNull;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HttpMocksSteps {
@@ -21,13 +23,22 @@ public class HttpMocksSteps {
     }
 
     @Given("^http mocks from '(.+)'$")
-    public void given_http_mocks_from_classpath_resource(@NonNull final String httpMocksFile) {
+    public void given_http_mocks_from_classpath_resource(@NonNull final String httpMocksFile) throws JSONException {
         final String serializedMappings = ClasspathUtils.loadFromClasspath(ResourceFilesVariables.getResourceFilesPrefix() + httpMocksFile);
 
-        final List<StubMapping> mappings = JsonUtils.parseStringToList(serializedMappings, StubMapping.class);
+        final List<StubMapping> mappings = new ArrayList<>();
 
-        httpMockServerManager.reset();
-        httpMockServerManager.register(mappings);
+        final JSONArray jsonArray = new JSONArray(serializedMappings);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            mappings.add(
+                    StubMapping.buildFrom(
+                            jsonArray.getJSONObject(i).toString()
+                    )
+            );
+        }
+
+        this.httpMockServerManager.reset();
+        this.httpMockServerManager.register(mappings);
     }
 
 }
